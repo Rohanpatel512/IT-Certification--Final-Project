0e7608bc2b7d2b1e7ef5d91d756c18d30e83d673
Rohanpatel512 <rohanpat223@gmail.com>
2023-08-17 13:31:03 -0400

    Finished writing script that modifies image size and type.

--- module/changeImage.py
+++ module/changeImage.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+
+# Imports
+import os
+import re
+from PIL import Image
+
+# Constants
+SAVE_PATH = "/supplier-data/images/"
+
+def change():
+ """
+ Modifies each image in directory
+ """
+
+ # Get the list of images in the directory
+ folder = os.listdir("/supplier-data/images")
+ 
+ # Loop through all images in the folder
+ for image_name in folder:
+    # Open each image
+    image = Image.open("/supplier-data/images/" + image_name)
+    
+    # Convert the image to a RGB 3-channel format
+    modified_image = image.convert('RGB')
+   
+    # Resize the image to 600x400 pixel
+    modified_image = modified_image.resize((600, 400))
+    
+    # Converts from .tiff to .jpeg
+    image_name = image_name + ".jpg"
+
+    # Set the new path of formatted image
+    directory = os.path.join(SAVE_PATH, image_name)
+   
+    # Save the formatted image to path
+    modified_image.save(directory)
+
+
+   
+    
+    
+ 
+ 

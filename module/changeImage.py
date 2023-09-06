602734918218f00683a13bf62b9804c169eb2a8a
Rohanpatel512 <rohanpat223@gmail.com>
2023-09-06 12:17:20 -0400

    Made small changes to code.

--- module/changeImage.py
+++ module/changeImage.py
@@ -6,23 +6,24 @@ import re
 from PIL import Image
 
 # Constants
-SAVE_PATH = "/supplier-data/images/"
+SAVE_PATH = "supplier-data/images/"
 
 def change():
  """
- Modifies each image in directory
+ Modifies each image in directory and then saves the image in the
+ same directory.
  """
 
  # Get the list of images in the directory
- folder = os.listdir("/supplier-data/images")
+ folder = os.listdir("supplier-data/images/")
  
  # Loop through all images in the folder
  for image_name in folder:
 
-   if image_name != ".DS_Store":
+   if image_name != ".DS_Store" and '.tiff' in image_name:
      
      # Open each image
-     image = Image.open("/supplier-data/images/" + image_name)
+     image = Image.open("supplier-data/images/" + image_name)
     
      # Convert the image to a RGB 3-channel format
      modified_image = image.convert('RGB')
@@ -31,13 +32,13 @@ def change():
      modified_image = modified_image.resize((600, 400))
 
      # Converts from .tiff to .jpg
-     image_name = image_name + ".jpg"
+     image_name = re.sub(r'.tiff', r'.jpeg', image_name)
 
      # Set the new path of formatted image
      directory = os.path.join(SAVE_PATH, image_name)
      
      # Save the formatted image to path
-     modified_image.save(directory)
+     modified_image.save(directory, "JPEG")
    
 change()
     
